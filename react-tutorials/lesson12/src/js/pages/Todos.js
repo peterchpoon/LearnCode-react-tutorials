e19eadff68f74ffc9d7201573147778692a18307
PC <PC>
2017-06-16 20:21:06 -0700

    Lesson 12 codebase

---
+++ react-tutorials/lesson12/src/js/pages/Todos.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import Todo from "../components/Todo";
+import * as TodoActions from "../actions/TodoActions";
+import TodoStore from "../stores/TodoStore";
+
+export default class Todos extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: TodoStore.getAll(),
+    };
+  }
+
+  componentWillMount(){
+    TodoStore.on("change", () =>{
+      this.setState({
+        todos: TodoStore.getAll(),
+      })
+    });
+  }
+
+  createTodo(){
+    TodoActions.createTodo(Date.now());
+  }
+
+  render(){
+    const { todos } = this.state;
+
+    const TodoComponents = todos.map((todo) => {
+      return <Todo key={todo.id} {...todo} />;
+    });
+
+
+    return (
+      <div>
+        <button onClick={this.createTodo.bind(this)}>Create!</button>
+        <h1>Todos</h1>
+        <ul>{TodoComponents}</ul>
+      </div>
+    );
+  }
+}
